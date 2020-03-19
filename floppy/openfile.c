a05a655926b208e3dc93b878e8eb6deafff2602d
John Criswell <jtcriswel@gmail.com>
2020-03-18 22:11:36 -0400

    Finished skeleton code for openfile.
    Finished the first draft of the skeleton code to give to students
    for MP3.  It contains all of the code except the code which they
    will need to write.

--- floppy/openfile.c
+++ floppy/openfile.c
@@ -5,8 +5,10 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
 
 /* Define constants for the three user IDs */
@@ -38,9 +40,80 @@ static const uid_t judy = 30;
  */
 int
 main (int argc, char ** argv) {
+	/* File descriptor for the file we will open */
+	int fd;
+
+	/* Length of data read from file */
+	int length;
+
+	/* Buffer into which to read data */
+	unsigned char buffer[1024];
+
+	/* Buffer to write to file */
+	unsigned char * output = "Mighty Mighty Bosstones\n";
+
+	/*
+	 * Ensure that the program was executed with the correct number of
+	 * arguments.  If not, terminate early.
+	 */
+	if (argc != 2) {
+		fprintf (stderr, "Usage: %s <pathname>\n", argv[0]);
+		return 1;
+	}
+
+	/*
+	 * Set the group IDs.  The choice of 100 is arbitrary.
+	 */
+	if ((setresgid (100, 100, 100)) == -1) {
+		perror ("Failed to set group IDs");
+		return 1;
+	}
+
 	/*
 	 * Simulate executing two setuid binaries as described in Quiz 3.
 	 */
-	setresuid (john, judy, jim);
+	if ((setresuid (john, judy, jim)) == -1) {
+		perror ("Failed to set user IDs");
+		return 1;
+	}
+
+	/*
+	 * TODO: Students should insert their code here so that the subsequent
+	 * call to open() succeeds.
+	 */
+
+	/*
+	 * Open the file.
+	 */
+	if ((fd = open (argv[1], O_RDWR)) == -1) {
+		perror ("Failed to open file");
+		return 1;
+	} else {
+		/*
+		 * Read data from the file and print it to the screen.  We
+		 * flush the standard output file pointer so that a simple call
+		 * to the write() system call to the standard output file
+		 * descriptor will not rearrange the output due to buffering.
+		 */
+		if ((length = read (fd, buffer, sizeof (buffer))) == -1) {
+			perror ("Failed to read file");
+			return 1;
+		}
+		fflush (stdout);
+		write (STDOUT_FILENO, buffer, length);
+
+		/*
+		 * Replace the contents of the file with new contents.
+		 */
+		if ((lseek (fd, 0, SEEK_SET)) == -1) {
+			perror ("Failed to seek to beginning of file");
+			return 1;
+		}
+
+		if ((write (fd, output, strlen (output))) == -1) {
+			perror ("Failed to read file");
+			return 1;
+		}
+	}
 	return 0;
 }
