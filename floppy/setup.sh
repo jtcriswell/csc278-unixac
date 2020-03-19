a320f383c8b18758dfa713f5eab9545f6d8e63a3
John Criswell <jtcriswel@gmail.com>
2020-03-18 23:28:52 -0400

    Enhanced and fixed setup.sh script.
    Fixed the permissions on one of the directories.
    Added code to modify /etc/passwd and /etc/group so that ls provides
    readable names.

--- floppy/setup.sh
+++ floppy/setup.sh
@@ -25,9 +25,17 @@ chmod 0100 /home
 chmod 0000 /home/dir1
 chmod 0000 /home/dir1/file
 
-chown 20:50 /home
 chown 10:50 /home/dir2
 chown 30:50 /home/dir2/file
 chmod 0100 /home
 chmod 0000 /home/dir2
 chmod 0000 /home/dir2/file
+
+#
+# Add entries to /etc/passwd and /etc/group to make output readable
+#
+echo "john:*:20:101:john:/:/bin/false" >> /etc/passwd
+echo "jim:*:10:101:jim:/:/bin/false" >> /etc/passwd
+echo "judy:*:30:101:judy:/:/bin/false" >> /etc/passwd
+
+echo "faculty:x:50:" >> /etc/group
